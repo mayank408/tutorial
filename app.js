3cbbb3e7e7883a806886112dc1f43843dfb5460a
Mayank <mayank.trp48@gmail.com>
2018-01-30 11:40:00 +0530

    add app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('A user connected');
+
+  //Send a message after a timeout of 4seconds
+  setTimeout(function(){
+    socket.send('Sent a message 4seconds after connection!');
+  }, 4000);
+  socket.on('chat message',function(data){
+    console.log(data);
+  });
+  socket.on('disconnect', function () {
+    console.log('A user disconnected');
+  });
+  socket.emit('Hello','Hello from the Server');
+});
+app.listen(3055, function(){
+  console.log('listening on *:3000');
+});
+
+
